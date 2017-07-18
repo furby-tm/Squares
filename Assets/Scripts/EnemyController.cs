c2bba88bab40cf6be133cd97e99770246ee368c2
Tyler Furby <tyler@tylerfurby.com>
2017-07-17 19:17:10 -0600

    Add another Enemy

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
 
-	public Transform farEnd;
+	 public Transform farEnd;
 	 private Vector3 frometh;
 	 private Vector3 untoeth;
 	 private float secondsForOneLength = 20f;
@@ -19,7 +19,7 @@ public class EnemyController : MonoBehaviour
 	 void Update()
 	 {
 	 transform.position = Vector3.Lerp(frometh, untoeth,
-	  Mathf.SmoothStep(0f,1f,
+	  Mathf.SmoothStep(0f,2f,
 	   Mathf.PingPong(Time.time/secondsForOneLength, 1f)
 	 ) );
 	 }
