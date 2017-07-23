38d5a800eea3194750432f3ee59e9ef58b371836
Tyler Furby <tyler@tylerfurby.com>
2017-07-23 02:47:09 -0600

    Change transform to rigibody to perserve physics

--- Assets/Scripts/DestroyController.cs
+++ Assets/Scripts/DestroyController.cs
@@ -7,15 +7,23 @@ public class DestroyController : MonoBehaviour {
 	public GameObject theCanvas;
 	public DeathCanvas canvas;
 	public GameObject bigExplosionEffect;
+	public GameObject player;
+	public Rigidbody rb;
+	public int MoveSpeed = 4;
+	public int MaxDist = 10;
+	public int MinDist = 1; 
 
 	// Use this for initialization
 	void Start () {
 		canvas = GetComponent<DeathCanvas>();
+		player = GameObject.FindGameObjectWithTag("Player");
+		rb = GetComponent<Rigidbody>();
 	}
 
 	// Update is called once per frame
-	void Update () {
-
+	void FixedUpdate () {
+		Vector3 direction = (player.transform.position - transform.position).normalized;
+		rb.MovePosition(transform.position + transform.forward * MoveSpeed * Time.deltaTime);
 	}
 
 	void OnTriggerEnter(Collider other)
@@ -24,6 +32,6 @@ public class DestroyController : MonoBehaviour {
 		{
 			DestroyObject(other.gameObject);
 			DeathCanvas.PlayerIsKilled = true;
-	  }
+	  	}
 	}
 }
